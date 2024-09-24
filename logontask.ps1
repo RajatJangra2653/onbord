41b5bd2cedf9a981360fa5d6d1d6adf007944c4a
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 17:40:57 +0530

    Update logontask.ps1

--- logontask.ps1
+++ logontask.ps1
@@ -11,12 +11,19 @@ sleep 180
 Get-Service -Name "com.docker.service"
 
 docker --version
+$dockerUsersGroupExists = Get-LocalGroup -Name 'docker-users' -ErrorAction SilentlyContinue
+$CurrentUser = "demouser"
+if ($dockerUsersGroupExists -ne $null) {
+    Add-LocalGroupMember -Group 'docker-users' -Member $CurrentUser -Verbose
+    Write-Host "User '$CurrentUser' added to the 'docker-users' group."
+} else {
+    Write-Host "'docker-users' group does not exist. Skipping adding the user to 'docker-users'."
+}
 
-docker ps
 
 Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 sleep 180
-
+docker ps
 
 docker pull hello-world
 
