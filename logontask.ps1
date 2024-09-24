dc4859d638b230cea48d17a94fe2822427732aab
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 20:04:45 +0530

    Update logontask.ps1

--- logontask.ps1
+++ logontask.ps1
@@ -1,22 +1,12 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension1.txt -Append
-
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 Set-ItemProperty -Path $regPath -Name "Docker Desktop" -Value "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-
 Start-Service -Name "com.docker.service"
-
-
-sleep 180
-
+sleep 60
 Get-Service -Name "com.docker.service"
-
 docker --version
-
 Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-sleep 180
+sleep 60
 docker ps
-
 docker pull hello-world
-
-
 Stop-Transcript
