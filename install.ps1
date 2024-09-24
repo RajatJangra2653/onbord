8319ca35ede9f05d0ae28a344d1799e5abbf548f
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 17:17:39 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,12 +8,12 @@ $webClient.DownloadFile($url, $outputFile)
 Start-Process $outputFile -Wait -ArgumentList 'install', '--accept-license', '--always-run-service', '--quiet', '--admin-settings'
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://mfastorageaccount8878.blob.core.windows.net/buildintellingentapps/logontask.ps1","C:\LabFiles\logontask-ia.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/RajatJangra2653/onbord/refs/heads/main/logontask.ps1","C:\LabFiles\logontask.ps1")
 
 
 $Trigger= New-ScheduledTaskTrigger -AtLogOn
 $User= "$($env:ComputerName)\demouser"
-$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\LabFiles\logontask-ia.ps1"
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\LabFiles\logontask.ps1"
 Register-ScheduledTask -TaskName "Setup" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
 Set-ExecutionPolicy -ExecutionPolicy bypass -Force
 
