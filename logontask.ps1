c6fe65fc087a7d8356ca93211732359decddb1a4
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 17:17:01 +0530

    Create logontask.ps1

---
+++ logontask.ps1
@@ -0,0 +1,18 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension1.txt -Append
+
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+Set-ItemProperty -Path $regPath -Name "Docker Desktop" -Value "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+Start-Service -Name "com.docker.service"
+Get-Service -Name "com.docker.service"
+docker --version
+
+docker ps
+
+Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+
+docker pull hello-world
+
+
+Stop-Transcript
