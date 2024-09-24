88f9d193fec791c13ee1dc699358df4637f4e819
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 20:46:25 +0530

    Update and rename logontask.ps1 to logontask1.ps1

--- logontask1.ps1
+++ logontask1.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path C:\WindowsAzure\Logs\CustomScriptExtension1.txt -Append
+Start-Transcript -Path C:\WindowsAzure\Logs\Logon1.txt -Append
 Write-Host "Custom Script Extention Started"
 $webClient = New-Object System.Net.WebClient
 $url = "https://desktop.docker.com/win/main/amd64/165256/Docker%20Desktop%20Installer.exe"
@@ -12,12 +12,4 @@ Write-Host "Docker Installed"
 sleep 60
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 Set-ItemProperty -Path $regPath -Name "Docker Desktop" -Value "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-Start-Service -Name "com.docker.service"
-sleep 60
-Get-Service -Name "com.docker.service"
-docker --version
-Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-sleep 60
-docker ps
-docker pull hello-world
 Stop-Transcript
