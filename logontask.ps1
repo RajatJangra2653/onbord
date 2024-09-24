d2ef38adf37b16e255cd4f99cc5532a7cd3b43c9
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-24 17:24:58 +0530

    Update logontask.ps1

--- logontask.ps1
+++ logontask.ps1
@@ -4,12 +4,18 @@ $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 Set-ItemProperty -Path $regPath -Name "Docker Desktop" -Value "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 Start-Service -Name "com.docker.service"
+
+
+sleep 180
+
 Get-Service -Name "com.docker.service"
+
 docker --version
 
 docker ps
 
 Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+sleep 180
 
 
 docker pull hello-world
