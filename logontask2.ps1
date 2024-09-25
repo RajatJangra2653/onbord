8d746fc451fe15bfc785ea5268a886e8556922f8
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-09-25 19:42:34 +0530

    Update logontask2.ps1

--- logontask2.ps1
+++ logontask2.ps1
@@ -5,7 +5,7 @@ Get-Service -Name "com.docker.service"
 docker --version
 Unregister-ScheduledTask -TaskName "Setup1" -Confirm:$false
 Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-sleep 60
+sleep 120
 docker ps
 docker pull hello-world
 Stop-Transcript
